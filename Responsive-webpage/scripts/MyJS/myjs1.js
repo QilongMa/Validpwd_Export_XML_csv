3c17d0fa9e861d3e0fb5f278f145b70a215a3bfd
Bruce <maljean2012@gmail.com>
2017-04-14 15:15:27 -0400

    Resonsive webpage

---
+++ Responsive-webpage/scripts/MyJS/myjs1.js
@@ -0,0 +1,16 @@
+﻿
+//JavaScript Version
+window.onload = function () {
+    if (document.addEventListener) {
+        document.getElementById('button1').addEventListener('click', clickHandler, false);
+    }
+    else {
+        document.getElementById('button1').attachEvent('onclick', clickHandler);
+    }
+};
+
+function clickHandler() {
+    alert('Simple jQuery');
+}
+
+//jQuery
